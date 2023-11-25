d29ca8dfa044ae1e9e138add45f93d43861d69b8
Reuben <treuben90@yahoo.com>
2023-11-26 00:34:23 +0200

    created the carousel for the landing page

--- HairBayFrontEnd/src/App.jsx
+++ HairBayFrontEnd/src/App.jsx
@@ -1,12 +1,13 @@
 import LandingPage from "./Pages/Landing Page/LandingPage";
 import SearchBar from "./Components/search Bar/SearchBar";
 import Button from "./Components/Button/Button";
+import { slides } from "../src/data/barber-carousel.json";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <LandingPage data={slides} />
     </>
   );
 }
--- HairBayFrontEnd/src/Pages/Landing Page/LandingPage.jsx
+++ HairBayFrontEnd/src/Pages/Landing Page/LandingPage.jsx
@@ -1,10 +1,60 @@
+import { useState, useEffect } from "react";
 import NavBar from "../../Components/NavBar/NavBar";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 import "./LandingPage.css";
 
-function LandingPage() {
+function LandingPage({ data }) {
+  const [slide, setSlide] = useState(0);
+  const nextSlide = () => {
+    setSlide(slide === data.length - 1 ? 0 : slide + 1);
+  };
+
+  const previousSlide = () => {
+    setSlide(slide === 0 ? data.length - 1 : slide - 1);
+  };
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      nextSlide();
+    }, 8000);
+    return () => clearInterval(intervalID);
+  });
   return (
     <div className="landing-page-container">
-      <NavBar />
+      <div className="landing-page-content">
+        {/* <NavBar /> */}
+        <BsArrowLeftCircleFill
+          className="arrow arrow-left"
+          onClick={previousSlide}
+        />
+        {data.map((item, i) => {
+          return (
+            <img
+              src={item.src}
+              alt={item.alt}
+              key={i}
+              className={slide === i ? "slide" : "slide slide-hidden"}
+            />
+          );
+        })}
+        <BsArrowRightCircleFill
+          className="arrow arrow-right"
+          onClick={nextSlide}
+        />
+        <span className="indicators">
+          {data.map((_, i) => {
+            return (
+              <button
+                key={i}
+                onClick={() => setSlide(i)}
+                className={
+                  slide === i ? "indicator" : "indicator indicator-disabled"
+                }
+              ></button>
+            );
+          })}
+        </span>
+      </div>
     </div>
   );
 }
