38126a5531cd7093601c468ab6d1dcad42a9131d
Reuben <treuben90@yahoo.com>
2023-11-27 23:29:11 +0200

    created the user cards with its user profiles

--- HairBayFrontEnd/src/Components/NameAndImage/NameAndImage.jsx
+++ HairBayFrontEnd/src/Components/NameAndImage/NameAndImage.jsx
@@ -4,6 +4,11 @@ import "./NameAndImage.css";
 function NameAndImage() {
   return (
     <div className="nameAndImage-container">
+      <div className="name-location">
+        <h5 className="full-name">Benjamin Cole</h5>
+        <p className="location">City, Country</p>
+        <p className="customers">12000 customers</p>
+      </div>
       <div className="nameImage-content">
         <img src={profile} alt="" className="profile-content" />
       </div>
