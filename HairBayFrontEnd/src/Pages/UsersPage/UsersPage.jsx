80bd2de63d88d80a68a9d285600e1f73b0d2d45c
Reuben <treuben90@yahoo.com>
2023-12-01 14:56:03 +0200

    created the sorting for the users page

--- HairBayFrontEnd/src/Pages/UsersPage/UsersPage.jsx
+++ HairBayFrontEnd/src/Pages/UsersPage/UsersPage.jsx
@@ -3,16 +3,17 @@ import { useState } from "react";
 import UserCards from "../../Components/UserCards/UserCards";
 import "./UsersPage.css";
 
-const categories = ["Unisex", "Male", "Female"];
+const categories = ["All Stylists", "Unisex", "Male", "Female"];
 function UsersPage({ users }) {
+  console.log(users.map((user) => user.category));
   const [activeCategory, setActiveCategory] = useState(0);
+  const [user, setUser] = useState();
+  console.log(activeCategory);
+  console.log(user);
   return (
     <div className="users-page-container">
       <NavBar classNameChange="newNav-bar" />
       <div className="users-page-sort">
-        {/* <SortBy category="Unisex" />
-        <SortBy category="Male" />
-        <SortBy category="Female" /> */}
         {categories.map((category, i) => {
           return (
             <div
@@ -30,7 +31,16 @@ function UsersPage({ users }) {
         })}
       </div>
       <div className="rendered-users">
-        <UserCards users={users} />
+        {users
+          .filter(
+            (user) =>
+              user.category === categories[activeCategory] ||
+              categories[activeCategory] === 0
+          )
+          .map((user, i) => {
+            return <UserCards users={users} key={i} />;
+          })}
+        {/* <UserCards users={users} /> */}
       </div>
     </div>
   );
