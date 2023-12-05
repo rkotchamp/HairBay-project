8958e5d2d637011eb33f60fe702d24e426b783ea
Reuben <treuben90@yahoo.com>
2023-12-05 13:54:29 +0200

    created the user expect and location tab

--- HairBayFrontEnd/src/Pages/Profile Page/Profile.jsx
+++ HairBayFrontEnd/src/Pages/Profile Page/Profile.jsx
@@ -1,6 +1,8 @@
 import ProfileAvatar from "../../assets/christopher-campbell-rDEOVtE7vOs-unsplash.jpg";
 import { FaAngleLeft } from "react-icons/fa";
 import Button from "../../Components/Button/Button";
+import { MdLocationOn } from "react-icons/md";
+import { BsFillPeopleFill } from "react-icons/bs";
 
 import "./Profile.css";
 
@@ -25,7 +27,26 @@ function Profile() {
           <Button text="Message" />
           <Button text="Book Now" />
         </div>
-        <div className="profile-info"></div>
+        <div className="profile-info">
+          <div className="location">
+            <span>
+              <MdLocationOn />
+            </span>
+            <p>Manhattan,USA</p>
+          </div>
+          <div className="type-hairstyle">
+            <span>
+              <BsFillPeopleFill />
+            </span>
+            <p>Unisex Hairstylist</p>
+          </div>
+          <div className="type-hairstyle">
+            <span>
+              <BsFillPeopleFill />
+            </span>
+            <p>Unisex Hairstylist</p>
+          </div>
+        </div>
       </div>
     </div>
   );
