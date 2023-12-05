bc8d61559eafce54dd870c43e18dfc149275c940
Reuben <treuben90@yahoo.com>
2023-12-05 13:40:23 +0200

    created the user profile and avatar

--- HairBayFrontEnd/src/Pages/Profile Page/Profile.jsx
+++ HairBayFrontEnd/src/Pages/Profile Page/Profile.jsx
@@ -1,22 +1,31 @@
 import ProfileAvatar from "../../assets/christopher-campbell-rDEOVtE7vOs-unsplash.jpg";
 import { FaAngleLeft } from "react-icons/fa";
+import Button from "../../Components/Button/Button";
 
 import "./Profile.css";
 
 function Profile() {
   return (
     <div className="profile-container">
-      <div className="profile-header">
-        <FaAngleLeft className="arrowBack" />
-      </div>
       <div className="profile-content">
-        <div className="profile--avatar">
-          <div className="light--background light--one">
-            <div className="light--background light--two">
-              {/* <img src={ProfileAvatar} alt="" className="avatarProfile" /> */}
+        <div className="profile-header">
+          <FaAngleLeft className="arrowBack" />
+        </div>
+        <div className="user-header">
+          <div className="profile--avatar">
+            <div className="light--background light--one">
+              <div className="light--background light--two">
+                <img src={ProfileAvatar} alt="" className="avatarProfile" />
+              </div>
             </div>
           </div>
+          <h3 className="user-profile-name">Benjamin Cutz</h3>
         </div>
+        <div className="profiles-call-to-actions">
+          <Button text="Message" />
+          <Button text="Book Now" />
+        </div>
+        <div className="profile-info"></div>
       </div>
     </div>
   );
